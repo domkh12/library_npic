e39e618c64bb3d965bf0df609ab7c922aa9e5a89
EI CHANUDOM <chanudomei122@gmail.com>
2024-06-30 16:03:04 +0700

    Your commit message

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,19 @@ Route::put("/attendent/{attendentId}", [AttendentController::class, 'update'])->
 Route::delete("/attendent/{attendentId}", [AttendentController::class, 'destroy'])->name('attendent.delete');
 
 Route::get('/attendent/{cateId}', [AttendentController::class, 'show'])->name("attendent.show");
+
+use App\Http\Controllers\BookController;
+
+Route::get('/book', [BookController::class, 'index'])->name("book.list");
+
+Route::get('/book/create', [BookController::class, 'create'])->name("book.create");
+
+Route::post('/book', [BookController::class, 'store'])->name("book.store");
+
+Route::get("/book/{bookId}/edit", [BookController::class, 'edit'])->name('book.edit');
+
+Route::put("/book/{bookId}", [BookController::class, 'update'])->name('book.update');
+
+Route::delete("/book/{bookId}", [BookController::class, 'destroy'])->name('book.delete');
+
+Route::get('/book/{cateId}', [BookController::class, 'show'])->name("book.show");
