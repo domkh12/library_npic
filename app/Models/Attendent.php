bdc56659a5564471b440c9d95712e04bbc72b179
naikong <naik18909@gmail.com>
2024-06-30 15:00:35 +0700

    Your commit message

--- app/Models/Attendent.php
+++ app/Models/Attendent.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Attendent extends Model
 {
     use HasFactory;
+    protected $table = '    ';
+    protected $fillable = [
+        'name',
+        'stu_id',
+        'time_out',
+        'date',
+        'status',
+    ];
+
 }
