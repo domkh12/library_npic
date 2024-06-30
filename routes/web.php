8485f0b627c470b876eb1a4a2e3c68236c7e5d30
EI CHANUDOM <chanudomei122@gmail.com>
2024-06-30 13:28:06 +0700

    create table

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/testuser',function(){
+    $user = DB::table('users')->where('id', 1)->first();
+    dd($user);
 });
