134416256874ff1f8f10f85930528c9e61362209
EI CHANUDOM <chanudomei122@gmail.com>
2024-06-30 16:01:56 +0700

    Your commit message

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
-use App\Http\Controllers\AttendentController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +12,19 @@ Route::get('/testuser', function () {
     $user = DB::table('users')->where('id', 1)->first();
     dd($user);
 });
+
+use App\Http\Controllers\AttendentController;
+
+Route::get('/attendent', [AttendentController::class, 'index'])->name("attendent.list");
+
+Route::get('/attendent/create', [AttendentController::class, 'create'])->name("attendent.create");
+
+Route::post('/attendent', [AttendentController::class, 'store'])->name("attendent.store");
+
+Route::get("/attendent/{attendentId}/edit", [AttendentController::class, 'edit'])->name('attendent.edit');
+
+Route::put("/attendent/{attendentId}", [AttendentController::class, 'update'])->name('attendent.update');
+
+Route::delete("/attendent/{attendentId}", [AttendentController::class, 'destroy'])->name('attendent.delete');
+
+Route::get('/attendent/{cateId}', [AttendentController::class, 'show'])->name("attendent.show");
