896d4d15c1ec9d88fc303ea764fdcec3b430e54b
EI CHANUDOM <chanudomei122@gmail.com>
2024-06-30 20:51:27 +0700

    Your commit message

--- resources/views/layout/backend.blade.php
+++ resources/views/layout/backend.blade.php
@@ -69,12 +69,7 @@
             </div>
             <div id="layoutSidenav_content">
                 <main>
-                    <div class="container-fluid px-4">
-                        <h1 class="mt-4">Static Navigation</h1>
-                        <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Static Navigation</li>
-                        </ol>
+                    <div class="container-fluid px-4 py-4">                        
                         <div class="card mb-4">
                             <div class="card-body">
                                 @yield('content')
