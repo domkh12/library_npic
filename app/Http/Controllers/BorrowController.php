a05cc7df1f79712eae2adfb50a9e1e449e8e8e8f
Nget vichetphanha <satokonkhmer@gmail.com>
2024-07-06 13:13:11 +0700

    Your commit message

--- app/Http/Controllers/BorrowController.php
+++ app/Http/Controllers/BorrowController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class BrrowController extends Controller
+class BorrowController extends Controller
 {
     /**
      * Display a listing of the resource.
